6c50c1bfe8d0baba4176639ce2eef7414bdd15b8
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-10-26 19:22:13 -0300

    Consertando o número de termos mostrados

--- curso_em_video/ex051_progressao_aritmetica.py
+++ curso_em_video/ex051_progressao_aritmetica.py
@@ -6,7 +6,9 @@ termo = int(input('Primeiro termo: '))
 razao = int(input('Razão: '))
 
 print(termo, ' -> ', end=' ')
-for n in range(10):
+
+# o range será 9 pois o usuário já passa o 1o termo, assim fazem 10 termos ao total
+for n in range(9):
     termo = termo + razao
     print(termo, ' -> ', end=' ')
 print('ACABOU')
