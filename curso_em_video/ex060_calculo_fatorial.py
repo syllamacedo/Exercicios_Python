6a74f96ebe77c155e30a8e978026a0b92ec482a3
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-10-30 19:12:16 -0300

    Ajustando a opção de fazer sem o uso da biblioteca

--- curso_em_video/ex060_calculo_fatorial.py
+++ curso_em_video/ex060_calculo_fatorial.py
@@ -1,15 +1,27 @@
 from math import factorial
 
 num = int(input('Digite um número para saber seu fatorial: '))
-print('Calculando o fatorial de {}! = '.format(num), end='')
-f = factorial(num)
 
-# FAZENDO DE FORMA MATEMÁTICA, SEM A AJUDA DO MATH
-# f = 1
+print('\nCalculando o fatorial de {}:'
+        '\n{}! = '.format(num, num), end='')
+
+# FAZENDO SEM A BIBLIOTECA MATH
+# o valor inicial da variavel f sera a variavel num
+# dentro do while devemos colocar um if
+
+# f = num
+f = factorial(num)
 
 while num > 0:
     print('{}'.format(num), end='')
     print(' x ' if num != 1 else ' = ', end='')
-    num -= 1
-    # f *= num
+    num -= 1 # deve ser retirado ao usar o if
+    
+#    if num != 1:
+#        num -= 1
+#        f *= num
+#    else:
+#        f *= num
+#        num -= 1
+
 print('{}'.format(f))
