7203404a0e0d5660c48c39c98b778891341e54fe
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-10-31 19:08:29 -0300

    Mudando a flag e mexendo na formatação

--- curso_em_video/ex064_soma_e_quantidade_numeros.py
+++ curso_em_video/ex064_soma_e_quantidade_numeros.py
@@ -1,7 +1,13 @@
-num = soma = cont = 0
-while num != 999:
+# o script recebe como entrada uma quantidade x de números
+# retorna a soma e quantidade de numeros recebidos
+
+num = cont = -1
+soma = 1
+
+while num != 0:
     soma += num
-    num = int(input('Digite um número [999 para parar]: '))
+    num = int(input('Digite um número inteiro [0 para parar]: '))
     cont += 1
-print('A soma dos números digitados é igual a {}.\n'
-      'A quantidade de números digitados foi {}.'.format(soma, cont - 1))
+
+print('\nA soma dos números digitados é igual a {}.\n'
+      'A quantidade de números digitados foi {}.'.format(soma, cont))
