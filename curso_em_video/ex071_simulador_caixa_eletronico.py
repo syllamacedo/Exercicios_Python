593c45cf843ee78eeebc6d01903f11ddfae2072e
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-11-02 19:08:08 -0300

    Modificações e adicionando explicações
    Adicionando variaveis para o valor das 4 notas disponibilizadas pelo caixa eletronico. Assim se quisermos mudar o valor das notas disponiveis, basta apenas mudar o valor das variaveis "nota"

--- curso_em_video/ex071_simulador_caixa_eletronico.py
+++ curso_em_video/ex071_simulador_caixa_eletronico.py
@@ -1,25 +1,52 @@
+# o script simula um caixa eletronico onde recebe como entrada o valor do saque
+# vai retornar com o total de notas necessarias para realizar o saque
+#
+# coloquei variaveis para o valor das notas, assim o programa pode ser adaptado mais facilmente
+# a qualquer nota que desejarmos
+
 print('=' * 30)
 print('{:^30}'.format('BANCO COMUM'))
 print('=' * 30)
+
+nota1 = 50
+nota2 = 20
+nota3 = 10
+nota4 = 1
+
+print(f'Notas disponíveis: {nota1} - {nota2} - {nota3} - {nota4}')
+
 valor = int(input('Qual valor você quer sacar? R$ '))
+print()
+
 while True:
-    cinquenta = valor // 50
-    print(f'Total de {cinquenta} cédulas de R$50.')
-    if valor % 50 == 0:
-        break
-    resto = valor - (cinquenta * 50)
-    vinte = resto // 20
-    if vinte > 0:
-        print(f'Total de {vinte} cédulas de R$20.')
-    if resto % 20 == 0:
-        break
-    resto = resto - (vinte * 20)
-    dez = resto // 10
-    if dez > 0:
-        print(f'Total de {dez} cédulas de R$10.')
-    if valor % 10 == 0:
-        break
-    resto = valor - ((cinquenta * 50) + (vinte * 20) + (dez * 10))
+
+    if valor >= nota1:
+        total_nota1 = valor // nota1
+        print(f'Total de {total_nota1} cédulas de R${nota1}.')
+        if valor % nota1 == 0:
+            break
+    else:
+        total_nota1 = 0
+
+    resto = valor - (total_nota1 * nota1)
+    total_nota2 = resto // nota2
+
+    if total_nota2 > 0:
+        print(f'Total de {total_nota2} cédulas de R${nota2}.')
+        if resto % nota2 == 0:
+            break
+
+    resto = resto - (total_nota2 * nota2)
+    total_nota3 = resto // nota3
+
+    if total_nota3 > 0:
+        print(f'Total de {total_nota3} cédulas de R${nota3}.')
+        if valor % nota3 == 0:
+            break
+
+    resto = valor - ((total_nota1 * nota1) + (total_nota2 * nota2) + (total_nota3 * nota3))
+
     if resto > 0:
-        print(f'Total de {resto} cédulas de R$1.')
+        print(f'Total de {resto} cédulas de R${nota4}.')
+
     break
