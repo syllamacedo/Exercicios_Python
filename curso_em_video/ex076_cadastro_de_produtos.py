f50951fb54df095f1dc806a90b89f41739d8c5a7
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-11-05 16:00:50 -0300

    Adicionando explicações e colocando o preço como float

--- curso_em_video/ex076_cadastro_de_produtos.py
+++ curso_em_video/ex076_cadastro_de_produtos.py
@@ -1,20 +1,26 @@
-print('-='*20)
-print('Programa de cadastro de Produtos.\n'
-      'INSIRA 5 PRODUTOS:')
-print('-='*20)
+# foi feita uma adaptação nesse exercicio, ao inves de ser uma tupla com valores predeterminados
+# o programa vai receber do usuario o cadastro do nome e preco de 5 produtos
+# ao final vai exibir a listagem dos mesmos
+
+print('-=' * 20)
+print(f'{"Programa de cadastro de Produtos":^40}')
+print('-=' * 20)
+
 lista = (str(input('1o Produto: ')),
-         str(input('Preço do Produto: ')),
+         float(input('Preço do Produto: ')),
          str(input('2o Produto: ')),
-         str(input('Preço do Produto: ')),
+         float(input('Preço do Produto: ')),
          str(input('3o Produto: ')),
-         str(input('Preço do Produto: ')),
+         float(input('Preço do Produto: ')),
          str(input('4o Produto: ')),
-         str(input('Preço do Produto: ')),
+         float(input('Preço do Produto: ')),
          str(input('5o Produto: ')),
-         str(input('Preço do Produto: ')))
-print('-'*40)
-print(f'{"LISTAGEM DE PREÇOS":40}')
-print('-'*40)
+         float(input('Preço do Produto: ')))
+
+print('-' * 40)
+print(f'{"LISTAGEM DE PREÇOS":^40}')
+print('-' * 40)
+
 for n in range(0, len(lista)):
     if n % 2 == 0:
         print(f'{lista[n]:.<30}', end='')
