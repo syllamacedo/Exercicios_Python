ac18770018a3f2b3fbbb562b405d638276389fdb
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-11-09 19:09:57 -0300

    Ajustando a formatação e adicionando comentários

--- curso_em_video/ex090_medias_com_dicionarios.py
+++ curso_em_video/ex090_medias_com_dicionarios.py
@@ -1,10 +1,17 @@
+# o script vai receber como entrada o nome e media de um aluno
+# ao final vai retornar o nome, a media e a situacao do aluno
+# media >= 7 esta aprovado
+# media < 7 esta reprovado
+
 aluno = {'Nome': str(input('Nome: '))}
 aluno['Média'] = float(input(f'Média de {aluno["Nome"]}: '))
+
 if aluno['Média'] >= 7:
     aluno['Situação'] = 'Aprovado'
 else:
     aluno['Situação'] = 'Reprovado'
 
 print('-='*25)
+
 for v, k in aluno.items():
-    print(f'{v} é igual a {k}')
+    print(f'{v}: {k}')
