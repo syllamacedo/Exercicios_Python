d3d51c4fecee742b7cbdbf9f6b47d678e3d69f70
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-11-21 11:30:16 -0300

    Mudando nome da variavel e adicionando comentário

--- curso_em_video/ex076_cadastro_de_produtos.py
+++ curso_em_video/ex076_cadastro_de_produtos.py
@@ -1,11 +1,14 @@
 # Exercício Python 076: Crie um programa que tenha uma tupla única com nomes de produtos e seus respectivos preços,
 # na sequência. No final, mostre uma listagem de preços, organizando os dados em forma tabular.
 
+# Mudei o exercício para pedir que o usuário entre com o nome e preço de 5 produtos
+# ao invés de ser uma tupla com valores pré-determinados no script
+
 print('-=' * 20)
 print(f'{"Programa de cadastro de Produtos":^40}')
 print('-=' * 20)
 
-lista = (str(input('1o Produto: ')),
+listagem = (str(input('1o Produto: ')),
          float(input('Preço do Produto: ')),
          str(input('2o Produto: ')),
          float(input('Preço do Produto: ')),
@@ -20,8 +23,8 @@ print('-' * 40)
 print(f'{"LISTAGEM DE PREÇOS":^40}')
 print('-' * 40)
 
-for n in range(0, len(lista)):
+for n in range(0, len(listagem)):
     if n % 2 == 0:
-        print(f'{lista[n]:.<30}', end='')
+        print(f'{listagem[n]:.<30}', end='')
     elif n % 2 == 1:
-        print(f'R${lista[n]:>7}')
+        print(f'R${listagem[n]:>7}')
