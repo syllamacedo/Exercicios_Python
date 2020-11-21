8bc1dd4ae431f95fb85e7ba0422f30b8a7606040
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-11-21 11:10:29 -0300

    Ajustes nas variaveis e adequação ao que foi pedido

--- curso_em_video/ex072_range_de_numeros_por_extenso.py
+++ curso_em_video/ex072_range_de_numeros_por_extenso.py
@@ -2,21 +2,16 @@
 # uma contagem por extenso, de zero até vinte.
 # Seu programa deverá ler um número pelo teclado (entre 0 e 20) e mostrá-lo por extenso.
 
-numext = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
+num_extenso = ('zero', 'um', 'dois', 'três', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove', 'dez', 'onze', 'doze',
           'treze', 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove', 'vinte')
 
 while True:
+    num = int(input('Digite um número entre 0 e 20: '))
 
-    num = 22
-    while num not in range(21):
-        num = int(input('Digite um número entre 0 e 20: '))
-
-    print(f'Você digitou o número {numext[num].capitalize()}.')
-    print('-'*30)
-
-    escolha = ' '
-    while escolha not in 'SN':
-        escolha = str(input('Você quer continuar [S/N]? ')).strip().upper()[0]
-
-    if escolha == 'N':
+    if num in range(len(num_extenso)):
         break
+
+    print('Tente novamente!', end=' ')
+
+print('-'*30)
+print(f'Você digitou o número {num_extenso[num].capitalize()}.')
