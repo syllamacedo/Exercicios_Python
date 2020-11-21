5a899bc84e45acee633636ec881ff3cdcc19e019
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-11-21 11:34:40 -0300

    Adicionando comentário e melhorando o texto para o usuário

--- curso_em_video/ex077_contando_vogais_com_tuplas.py
+++ curso_em_video/ex077_contando_vogais_com_tuplas.py
@@ -1,12 +1,15 @@
 # Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos).
 # Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
+# Mudei o exercício para pedir que o usuário entre com 3 palavras para serem analisadas
+# ao invés de ser uma tupla com valores pré-determinados no script
+
 print(f'{"Contador de Vogais":^30}')
 print('-=' * 15)
 
-palavras = (str(input('Digite a 1a palavra: ')),
-            str(input('Digite a 2a palavra: ')),
-            str(input('Digite a 3a palavra: ')))
+palavras = (str(input('Digite a 1a palavra (não usar acentos): ')),
+            str(input('Digite a 2a palavra (não usar acentos): ')),
+            str(input('Digite a 3a palavra (não usar acentos): ')))
 
 for p in palavras:
     print(f'\nNa palavra {p.upper()} temos: ', end='')
