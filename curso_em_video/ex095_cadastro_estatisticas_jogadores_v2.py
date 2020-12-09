e630666cc9d6bbae7b6ad225c8fe0c7b136f0f40
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-12-09 16:42:55 -0300

    Melhorando a saída e renomeando variaveis

--- curso_em_video/ex095_cadastro_estatisticas_jogadores_v2.py
+++ curso_em_video/ex095_cadastro_estatisticas_jogadores_v2.py
@@ -1,39 +1,45 @@
+# Exercício Python 095: Aprimore o desafio 93 para que ele funcione com vários jogadores, 
+# incluindo um sistema de visualização de detalhes do aproveitamento de cada jogador.
+
 dados = dict()
 dadosfinal = []
 
 while True:
-    golspart = []
+    gols_partida = []
     dados['nome'] = str(input('Nome do jogador: ')).upper()
-    part = int(input('Quantas partidas ele jogou? '))
+    partida = int(input('Quantas partidas ele jogou? '))
 
-    for p in range(part):
-        golspart.append(int(input(f'    Quantos gols na partida {p+1}? ')))
+    for p in range(partida):
+        gols_partida.append(int(input(f'    Quantos gols na partida {p+1}? ')))
 
-    dados['gols'] = golspart[:]
+    dados['gols'] = gols_partida[:]
     dadosfinal.append(dados.copy())
 
     while True:
-        resp = str(input('Deseja continuar? [S/N] ')).upper()[0]
+        resposta = str(input('Deseja continuar? [S/N] ')).upper()[0]
 
-        if resp in 'SN':
+        if resposta in 'SN':
             break
         print('ERRO! Digite S para sim ou N para não.')
 
-    if resp == 'N':
+    if resposta == 'N':
         break
+    print()
 
 print('-='*30)
-print('cod          ', end=' ')
+print('cod', end=' ')
 
 for i in dados.keys():
     print(f'{i:<15}', end='')
 
 print()
+print('--' * 30)
+
 for k, v in enumerate(dadosfinal):
-    print(f'{(k+1):>3}', end='')
+    print(f'{k + 1:>3} ', end='')
 
     for d in v.values():
-        print(f'{str(d):>15}', end='')
+        print(f'{str(d):<15}', end='')
     print()
 
 while True:
