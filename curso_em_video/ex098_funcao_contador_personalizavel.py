68c9b66fd91bb16d81eea9ea81ff6104e700cf38
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-12-10 08:28:30 -0300

    Adicionando comentário explicativo do exercício e ajustando o passo
    Ajustando para não aceitar passo = 0, pois assim entrariamos em um loop.

--- curso_em_video/ex098_funcao_contador_personalizavel.py
+++ curso_em_video/ex098_funcao_contador_personalizavel.py
@@ -1,35 +1,44 @@
+# Exercício Python 098: Faça um programa que tenha uma função chamada contador(), que receba três parâmetros:
+# início, fim e passo.
+# Seu programa tem que realizar três contagens através da função criada:
+# a) de 1 até 10, de 1 em 1
+# b) de 10 até 0, de 2 em 2
+# c) uma contagem personalizada
+
 from time import sleep
 
 
 def contador(i, f, p):
+    if p == 0:
+        print('Zero não é aceito como passo. Tente novamente.')
+        return
+
     print('-=' * 20)
     print(f'Contagem de {i} até {f} de {p} em {p}')
 
     if p < 0:
         p *= -1
 
-    if p == 0:
-        p = 1
     cont = i
 
     if cont > f:
         while cont > f:
             print(f'{cont}... ', end='')
             cont -= p
-            sleep(0.5)
+            sleep(0.7)
     else:
         while cont <= f:
             print(f'{cont}... ', end='')
             cont += p
-            sleep(0.5)
+            sleep(0.7)
 
     print('FIM!')
 
 
-help(contador)
 contador(1, 10, 1)
 contador(10, 0, 2)
 
+print('-=' * 20)
 print('Agora é sua vez de personalizar a contagem!')
 
 ini = int(input('Início: '))
