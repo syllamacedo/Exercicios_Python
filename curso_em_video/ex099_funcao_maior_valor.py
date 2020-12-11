d9a9970d604682bebe87e60ec25a2feeecf13daf
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2020-12-11 15:03:02 -0300

    Adicionando comentário explicativo do exercicio e ajustando valores recebidos

--- curso_em_video/ex099_funcao_maior_valor.py
+++ curso_em_video/ex099_funcao_maior_valor.py
@@ -1,51 +1,36 @@
+# Exercício Python 099: Faça um programa que tenha uma função chamada maior(), que receba vários parâmetros
+# com valores inteiros. Seu programa tem que analisar todos os valores e dizer qual deles é o maior.
+
 from time import sleep
 
 
 def maior(*num):
-    maiornum = 0
+    contador = maiornum = 0
 
     print('-=' * 20)
     print('Analisando os valores passados...')
 
+    if num == ():
+        print('Não foram informados valores.')
+        return
+
     for n in num:
-        print(f'{n}', end=' ')
-        sleep(0.5)
-        if n >= maiornum:
+        print(f'{n}...', end=' ')
+        sleep(0.7)
+
+        if contador == 0:
             maiornum = n
+        else:
+            if n >= maiornum:
+                maiornum = n
+
+        contador += 1
 
     print(f'\nForam informados {len(num)} valores ao todo.')
     print(f'O maior valor informado foi {maiornum}.')
 
 
 maior(6)
+maior(-2, -9, -4, -5, -7, -1)
+maior(5, 15, 9, 0, -2)
 maior()  # Passando nenhum valor
-maior(2, 9, 4, 5, 7, 1)
-
-# FAZENDO COM LISTA E PEDINDO AO USUARIO QUE PREENCHA OS NÚMEROS:
-# Função vai ser a mesma, só vai mudar a variavel "num"
-
-''' 
-def maior(lst):
-    maior = 0
-    print('-=' * 20)
-    print('Analisando os valores passados...')
-    for n in lst:
-        print(f'{n}', end=' ')
-        sleep(0.5)
-        if n >= maior:
-            maior = n
-    print(f'\nForam informados {len(lst)} valores ao todo.')
-    print(f'O maior valor informado foi {maior}.')
-
-
-m = []
-while True:
-    m.append(int(input('Digite um número: ')))
-    resp = ' '
-    while resp not in 'SN':
-        resp = str(input('Deseja continuar [S/N]? ')).upper()
-    if resp == 'N':
-        break
-
-maior(m)
-'''
