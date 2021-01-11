bf2379e291129379d56b97094cf8d3b63339630a
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2021-01-11 17:20:11 -0300

    Adicionando comentário explicativo do exercício

--- curso_em_video/ex104_validando_entrada_dados_com_funcao.py
+++ curso_em_video/ex104_validando_entrada_dados_com_funcao.py
@@ -1,10 +1,14 @@
+# Exercício Python 104: Crie um programa que tenha a função leiaInt(), que vai funcionar de forma semelhante
+# ‘a função input() do Python, só que fazendo a validação para aceitar apenas um valor numérico.
+# Ex: n = leiaInt(‘Digite um n: ‘)
+
 def leiaint(num):
     while not num.isnumeric():
-        print('\033[0;31mERRO! Digite um número válido.\033[m')
-        num = (input('Digite um número: '))
+        print('\033[0;31mERRO! Digite um número inteiro válido.\033[m')
+        num = (input('Digite um número inteiro: '))
 
     return num
 
 
-n = leiaint(input('Digite um número: '))
+n = leiaint(input('Digite um número inteiro: '))
 print(f'Você acabou de digitar o número {n}.')
