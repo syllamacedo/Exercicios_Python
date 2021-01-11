1718923ac48876c8cd0586dee23077ba68e1cb9c
syllamacedo <67206564+syllamacedo@users.noreply.github.com>
2021-01-11 17:12:46 -0300

    Adicionando comentário explicativo e mudando nomes de variáveis

--- curso_em_video/ex103_ficha_jogador_com_funcao.py
+++ curso_em_video/ex103_ficha_jogador_com_funcao.py
@@ -1,17 +1,21 @@
-def ficha(jog='<desconhecido>', gol=0):
+# Exercício Python 103: Faça um programa que tenha uma função chamada ficha(), que receba dois parâmetros opcionais:
+# o nome de um jogador e quantos gols ele marcou. O programa deverá ser capaz de mostrar a ficha do jogador,
+# mesmo que algum dado não tenha sido informado corretamente.
+
+def ficha(jog='DESCONHECIDO', gol=0):
     print(f'O jogador {jog} fez {gol} gol(s) no campeonato.')
 
 
-print('--'*20)
-nome = str(input('Nome do jogador: ')).strip()
-gols = str(input('Número de gols: '))
+print('--' * 20)
+nome = str(input('Nome do jogador: ')).strip().upper()
+n_gols = str(input('Número de gols: '))
 
-if gols.isnumeric():
-    gols = int(gols)
+if n_gols.isnumeric():
+    n_gols = int(n_gols)
 else:
-    gols = 0
+    n_gols = 0
 
 if nome == '':
-    ficha(gol=gols)
+    ficha(gol=n_gols)
 else:
-    ficha(nome, gols)
+    ficha(nome, n_gols)
